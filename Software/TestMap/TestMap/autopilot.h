0041a15fee06a3b3697b9e516ce2f0b2085e9c63
Lin Weizhe Dragon <dvmsc110@gmail.com>
2018-10-09 16:41:07 +0100

    save

---
+++ Software/TestMap/TestMap/autopilot.h
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <map>
+#include <list>
+using namespace std;
+
+#define UP 0
+#define RIGHT 1
+#define DOWN 2
+#define LEFT 3
+
+#define GO_STRAIGHT 0
+#define TURN_LEFT -1
+#define TURN_RIGHT 1
+list<int> operation_list; // stores operations to be executed
+
+
+class Node{
+public:
+    string name="none";
+    Node* left=NULL;
+    Node* up=NULL;
+    Node* right=NULL;
+    Node* down=NULL;
+    void print(){
+        cout<<"name: "<<this->name<<endl;
+        if (this->left != NULL){
+            cout<<"left is:"<<this->left->name<<endl;
+        }
+        if (this->right!= NULL){
+            cout<<"right is:"<<this->right->name<<endl;
+        }
+        if (this->up!= NULL){
+            cout<<"up is:"<<this->up->name<<endl;
+        }
+        if (this->down!= NULL){
+            cout<<"down is:"<<this->down->name<<endl;
+        }
+        cout<<endl;
+    }
+};
+class Direction{
+public:
+    int direction = UP;
+    void updateDirection(int x){
+        this->direction+=x;
+        if (this->direction < 0)
+            this->direction = LEFT;
+        if (this->direction > 3)
+            this->direction = UP;
+        //-1 to turn left, 1 to turn right
+    }
+    int getDirection(){
+        return this->direction;
+    }
+};
+Node S1, S2, A1, A2, B1, B2, B5, B6, B7, B8;
+Node C1, C2;
+Node D1, D2, D3, D4, D5, D6, D7, D8;
+Node E1, E2, E3, E4, E5, E6, E7, E8;
+
+// a map of Nodes
+map<string, Node*>node_storage;
+// current node
+Node* current_node;
+Node* previous_node;
--- Software/TestMap/TestMap/main.cpp
+++ Software/TestMap/TestMap/main.cpp
@@ -7,78 +7,31 @@
 //
 
 #include <iostream>
+#include "autopilot.h"
 using namespace std;
-int current_color = 0; // current color of the brick that's going to cath
-int motor_power[3] = {0, 0, 0}; // hand left right motor
-int motor_angle[3] = {0, 0, 0};
-int left_wheel_reading = 0;
-int right_wheel_reading = 0;
 
-class FieldMap{
-public:
-    string name="none";
-    FieldMap* left=NULL;
-    FieldMap* up=NULL;
-    FieldMap* right=NULL;
-    FieldMap* down=NULL;
-    void print(){
-        if (this->left != NULL){
-            cout<<"left is:"<<this->left->name<<endl;
-        }
-        if (this->right!= NULL){
-            cout<<"right is:"<<this->right->name<<endl;
-        }
-    }
-};
 
-void motor(int power, int angle, int index){
-    // controls the motor at the hand of the robot
-    motor_power[index] = power;
-    motor_angle[index] = angle;
-}
-
-int eye_sensor_reading(void){
-    // use different int for different colors
-    return 0;
-}
-
-bool catch_failure(void){
-    // at failure, catch_brick can be called several times
-    return (eye_sensor_reading() != current_color);
-}
-
-void catch_brick(void){
-    motor(0, 0, 0);
-}
-
-void path_adjustment(void){
-    int adjustment_power_increment = 0; // some values found out in experiment
-    int adjustment_angle_used = 0;
-    int addressed_motor = 1;
-    if (left_wheel_reading > 0) // detects the white line
-        addressed_motor = 2;
-    if (left_wheel_reading > 0 || right_wheel_reading > 0){
-        int temp_power = motor_power[addressed_motor];
-        int temp_angle = motor_angle[addressed_motor];
-        motor(temp_power + adjustment_power_increment, adjustment_angle_used, addressed_motor);
-        motor(temp_power, temp_angle - adjustment_angle_used, addressed_motor);
-    }
-}
-
-void path_retrack(void){
-    // called when no crossing is detected when angle more than enough has been rotated
-    // currently no idea how to retrack. more information is needed
+void InitializeNode(string name, Node* target, Node* left, Node* up, Node* right, Node* down){
+    cout<<up<<endl;
+    target->name = name;
+    target->left = left;
+    target->up = up;
+    //cout<<(string)typeof(target->up) <<endl;
+    target->right = right;
+    target->down = down;
 }
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, World!\n";
-    FieldMap A,B;
-    A.name = "This is A";
-    B.name = "This is B";
-    A.left = &B;
-    B.right = &A;
-    A.print();
-    B.print();
+    A2.name = "A2";
+    
+    //////////////////////////left//up//right/down//////
+    cout<<&S2<<endl;
+    InitializeNode("S2", &S2, NULL, NULL, &A2, &S1);
+    InitializeNode("S1", &S1, NULL, &S2, &A1, NULL);
+    InitializeNode("A2", &A2, &S2, NULL, &B2, &A1);
+    InitializeNode("A1", &A1, &S1, &A2, &B1, NULL);
+    
+    
+    
     return 0;
 }
