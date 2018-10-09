a1a5ae6397f00867e4a3789a68bcc4cd2ead2957
Lin Weizhe Dragon <dvmsc110@gmail.com>
2018-10-09 21:03:11 +0100

    Complete Map Initialization

--- Software/TestMap/TestMap/main.cpp
+++ Software/TestMap/TestMap/main.cpp
@@ -10,28 +10,59 @@
 #include "autopilot.h"
 using namespace std;
 
-
+/*
+ Initialize a node, store into storage
+ */
 void InitializeNode(string name, Node* target, Node* left, Node* up, Node* right, Node* down){
-    cout<<up<<endl;
     target->name = name;
     target->left = left;
     target->up = up;
-    //cout<<(string)typeof(target->up) <<endl;
     target->right = right;
     target->down = down;
+    node_storage[name] = target;
 }
 
 int main(int argc, const char * argv[]) {
-    A2.name = "A2";
     
-    //////////////////////////left//up//right/down//////
-    cout<<&S2<<endl;
+    // map initialization
     InitializeNode("S2", &S2, NULL, NULL, &A2, &S1);
     InitializeNode("S1", &S1, NULL, &S2, &A1, NULL);
+    
     InitializeNode("A2", &A2, &S2, NULL, &B2, &A1);
     InitializeNode("A1", &A1, &S1, &A2, &B1, NULL);
     
+    InitializeNode("B2", &B2, &A2, &B5, &C2, &B1);
+    InitializeNode("B1", &B1, &A1, &B2, &C1, NULL);
+    InitializeNode("B5", &B5, NULL, &B6, &D5, &B2);
+    InitializeNode("B6", &B6, NULL, &B7, &D6, &B5);
+    InitializeNode("B7", &B7, NULL, &B8, &D7, &B6);
+    InitializeNode("B8", &B8, NULL, NULL, &D8, &B7);
     
+    InitializeNode("C2", &C2, &B2, NULL, &D2, &C1);
+    InitializeNode("C1", &C1, &B1, &C2, &D1, NULL);
+    
+    InitializeNode("D1", &D1, &C1, &D2, &E1, NULL);
+    InitializeNode("D2", &D2, &C2, &D3, &E2, &D1);
+    InitializeNode("D3", &D3, NULL, &D4, &E3, &D2);
+    InitializeNode("D4", &D4, NULL, &D5, &E4, &D3);
+    InitializeNode("D5", &D5, &B5, &D6, &E5, &D4);
+    InitializeNode("D6", &D6, &B6, &D7, &E6, &D5);
+    InitializeNode("D7", &D7, &B7, &D8, &E7, &D6);
+    InitializeNode("D8", &D8, &B8, NULL, &E8, &D7);
+    
+    InitializeNode("E1", &E1, &D1, &E2, NULL, NULL);
+    InitializeNode("E2", &E2, &D2, &E3, NULL, &E1);
+    InitializeNode("E3", &E3, &D3, &E4, NULL, &E2);
+    InitializeNode("E4", &E4, &D4, &E5, NULL, &E3);
+    InitializeNode("E5", &E5, &D5, &E6, NULL, &E4);
+    InitializeNode("E6", &E6, &D6, &E7, NULL, &E5);
+    InitializeNode("E7", &E7, &D7, &E8, NULL, &E6);
+    InitializeNode("E8", &E8, &D8, NULL, NULL, &E7);
+    
+    map<string, Node*>::iterator  iter;
+    for(iter = node_storage.begin(); iter != node_storage.end(); iter++){
+        iter->second->print();
+    }
     
     return 0;
 }
--- Software/autopilot.h
+++ Software/autopilot.h
@@ -30,10 +30,10 @@ public:
             cout<<"right is:"<<this->right->name<<endl;
         }
         if (this->up!= NULL){
-            cout<<"up is:"<<this->right->name<<endl;
+            cout<<"up is:"<<this->up->name<<endl;
         }
         if (this->down!= NULL){
-            cout<<"down is:"<<this->right->name<<endl;
+            cout<<"down is:"<<this->down->name<<endl;
         }
         cout<<endl;
     }
@@ -53,6 +53,10 @@ public:
         return this->direction;
     }
 };
+Node S1, S2, A1, A2, B1, B2, B5, B6, B7, B8;
+Node C1, C2;
+Node D1, D2, D3, D4, D5, D6, D7, D8;
+Node E1, E2, E3, E4, E5, E6, E7, E8;
 
 // a map of Nodes
 map<string, Node*>node_storage;
